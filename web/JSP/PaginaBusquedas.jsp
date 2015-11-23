76b3c238748b435401f63a89eeac310211b70d50
Queky <inakisanchez_92@hotmail.com>
2015-11-23 19:18:05 +0100

    paginas con validaciones

---
+++ web/JSP/PaginaBusquedas.jsp
@@ -0,0 +1,31 @@
+<%-- 
+    Document   : PaginaInicio
+    Created on : 18-nov-2015, 23:10:31
+    Author     : Iñaki
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>EuskalRent</title>
+        <link rel="stylesheet" href="../CSS/Estilo.css">
+        <link href='http://fonts.googleapis.com/css?family=Lato:400,700,400italic' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Vollkorn:700italic,400' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <header>
+            <a href="PaginaInicio.jsp" class="logo">
+                <em>Euskal</em><strong>Rent</strong>
+            </a>
+            <nav>
+                <ul>
+                    <li><a href="#">Acceder</a></li>
+                    <li><a href="#">Registrarse</a></li>
+                </ul>      
+            </nav>
+        </header>
+    </body>
+</html>
--- web/JavaScript/ValidacionBusqueda.js
+++ web/JavaScript/ValidacionBusqueda.js
@@ -3,18 +3,46 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-window.addEventListener("load", inicio, false);
-
-function inicio(){
-    document.getElementById("btnBuscar").addEventListener("click", validacion, false);
+window.onload = function() {
+    // Comprobacion numero de huespedes
+    var huespedes = document.getElementById("numHuespedes");
+    var exp = /[1-9]/;
+    huespedes.oninput = function() {
+        if(this.value>10 || !exp.test(this.value)){
+            huespedes.className='error';
+        } else {
+            huespedes.className='cajasDatos';
+        }
+    }
+    
+    var fechaInicio = document.getElementById("fechaInicio");
+    fechaInicio.oninput = function() {
+        var hoy = new Date();
+        hoy.getDate();
+        var fechaUsuario = new Date(fechaInicio.value);
+        if(hoy>fechaUsuario){
+            if(hoy.getDate()==fechaUsuario.getDate())
+                fechaInicio.className='cajasDatos';
+            else
+                fechaInicio.className='error';
+        } else {
+            fechaInicio.className='cajasDatos';
+        }
+    }
+    
+    var fechaFin = document.getElementById("fechaFin");
+    fechaFin.oninput = function() {
+        var hoy = new Date();
+        hoy.getDate();
+        var fechaUsuario = new Date(fechaFin.value);
+        if(hoy>fechaUsuario){
+            if(hoy.getDate()==fechaUsuario.getDate())
+                fechaFin.className='cajasDatos';
+            else
+                fechaFin.className='error';
+        } else {
+            fechaFin.className='cajasDatos';
+        }
+    }
 }
 
-function validacion() {
-    var barrio = document.getElementById("barrio").value;
-    alert(barrio);
-    /* para ejecutar el action definido en el formulario:
-     * 
-     * document.getElementById("formBusqueda").submit();
-    */
-}
