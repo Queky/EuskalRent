6baac987f2a6cdb5110948ff3c2fd2cd628d3ec9
zigorlopezsanpelayo <zigorlopezsanpelayo@gmail.com>
2015-11-24 22:55:28 +0100

    a

--- web/JavaScript/webStorage.js
+++ web/JavaScript/webStorage.js
@@ -20,14 +20,16 @@ function clickGuardarMA() {
     sessionStorage.setItem("ModificacionApartamento",datosSS);
 }
 
+/*
 var datos = JSON.parse(localStorage.getItem('RegistroApartamento'));
 
 function getDatosRegProp() {
-    var barrioRegistrado = datos.barrio;
-    var tipoApartReg = datos.tipoProp;
-    var numHueReg = datos.numHuespedes;
+    //var barrioRegistrado = datos.barrio;
+    //var tipoApartReg = datos.tipoProp;
+    //var numHueReg = datos.numHuespedes;
 }
+*/
 
-http://samcroft.co.uk/2013/using-localstorage-to-store-json/
-http://www.aprenderaprogramar.es/index.php?option=com_content&view=article&id=888:sessionstorage-y-localstorage-javascript-diferencias-guardar-datos-en-cache-y-persistencia-cu01198e&catid=78:tutorial-basico-programador-web-javascript-desde-&Itemid=206
+
+
 
